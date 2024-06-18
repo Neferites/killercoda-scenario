ab0c15367c0b7394a558b98df45e3c16e3fc82b0
Neferites <laurent.alhossri@gmail.com>
2024-06-18 11:48:53 +0200

    fix tls secret

--- services/background.sh
+++ services/background.sh
@@ -2,7 +2,7 @@
 
 set -x # to test stderr output in /var/log/killercoda
 
-kubectl create secret tls-cert --from-file=tls/
+kubectl create secret tls tls-cert --from-file=tls/
 kubectl create configmap nginx-proxy-conf --from-file=proxy.conf
 kubectl apply -f pods/healthy-monolith.yaml
 kubectl apply -f pods/pod-monolith.yaml
