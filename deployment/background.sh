ca61ee176305389b51cbc2ab2501ebb4c0102c56
Neferites <laurent.alhossri@gmail.com>
2024-06-18 16:07:35 +0200

    fix: cm

--- deployment/background.sh
+++ deployment/background.sh
@@ -3,4 +3,4 @@
 set -x # to test stderr output in /var/log/killercoda
 
 kubectl create secret generic tls-certs --from-file=tls/
-kubectl create configmap nginx-proxy-conf --from-file=configuration/nginx
+kubectl create configmap nginx-frontend-conf --from-file=configuration/nginx
