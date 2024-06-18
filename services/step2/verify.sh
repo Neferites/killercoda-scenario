c0c4a0ab2f3c1b2a180cc175084209165071274e
Neferites <laurent.alhossri@gmail.com>
2024-06-18 14:16:44 +0200

    fix: test step2

--- services/step2/verify.sh
+++ services/step2/verify.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-set -x 
 
-TEST=$(k get endpoints monolith -o jsonpath='{.subsets[0].addresses[0].ip}')
-if [[ -z "$TEST" ]]; then
-  return 1
-else
-  return 0
-fi
+k get endpoints monolith -o jsonpath='{.subsets[0].addresses[0].ip}' | grep '^[[:digit:]]'
