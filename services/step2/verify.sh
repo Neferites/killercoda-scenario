28f600ce4eaedac91b76c1087bd0bb6a527ed3a6
Neferites <laurent.alhossri@gmail.com>
2024-06-18 14:30:45 +0200

    gne

--- services/step2/verify.sh
+++ services/step2/verify.sh
@@ -2,4 +2,5 @@
 
 set -x # to test stderr output in /var/log/killercoda
 
-k get endpoints monolith -o jsonpath='{.subsets[0].addresses[0].ip}' | grep '192.168.0.8'
+k get endpoints monolith -o jsonpath='{.subsets[0].addresses[0].ip}' > /tmp/res-step2
+grep '192.168.0.8' /tmp/res-step2
