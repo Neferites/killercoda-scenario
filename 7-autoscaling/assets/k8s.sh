787162dedfdbefd30471b775e66b3d45bd9f9ff9
Neferites <laurent.alhossri@gmail.com>
2024-06-20 22:48:29 +0200

    .

--- 7-autoscaling/assets/k8s.sh
+++ 7-autoscaling/assets/k8s.sh
@@ -16,5 +16,9 @@ while ! kubectl get nodes | grep -w "Ready"; do
   sleep 1
 done
 
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+echo "alias k=kubectl" >> ~/.bashrc
+echo "complete -o default -F __start_kubectl k" >> ~/.bashrc
+echo "alias k9s=k9s --kubeconfig /etc/rancher/k3s/k3s.yaml" >> ~/.bashrc
 
 touch /ks/.k8sfinished
